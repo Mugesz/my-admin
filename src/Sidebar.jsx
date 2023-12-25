c06bf6ed573c8048b030c380101f56857d033f47
frekerzzzz <kannamugesh135@gmail.com>
2023-12-25 21:53:37 +0530

    m

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -6,47 +6,39 @@ const Sidebar = () => {
   const location = useLocation();
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <button
-        className="navbar-toggler"
-        type="button"
+    <div className="text-center">
+      <ul
+        className="navbar-nav  sidebar sidebar-dark accordion topsie text-center"
+        id="accordionSidebar"
       >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+        <Link
+          className="sidebar-brand d-flex align-items-center justify-content-center"
+          to="/"
+        >
+          <div className="sidebar-brand-text text-black-50 mx-3">ORDER</div>
+        </Link>
 
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav  sidebar sidebar-dark accordion topsie text-center">
-          <Link
-            className="sidebar-brand d-flex align-items-center justify-content-center"
-            to="/"
-          >
-            <div className="sidebar-brand-text text-white-50 mx-3">ORDER</div>
+        <hr className="sidebar-divider my-0" />
+        <hr className="sidebar-divider" />
+
+        <li className={`nav-item ${location.pathname === '/' ? 'active' : ''}`}>
+          <Link className="nav-link  " to="/">
+            <span className="orders">Order</span>
           </Link>
+        </li>
 
-          <hr className="sidebar-divider my-0" />
-          <hr className="sidebar-divider" />
+        <li className={`nav-item ${location.pathname === '/settings' ? 'active' : ''}`}>
+          <Link className="nav-link" to="/settings">
+            <span className="orders">Settings</span>
+          </Link>
+        </li>
 
-          <li className={`nav-item ${location.pathname === '/' ? 'active' : ''}`}>
-            <Link className="nav-link" to="/">
-              <span className="orders">Order</span>
-            </Link>
-          </li>
-
-          <li className={`nav-item ${location.pathname === '/settings' ? 'active' : ''}`}>
-            <Link className="nav-link" to="/settings">
-              <span className="orders">Settings</span>
-            </Link>
-          </li>
-
-          <li className={`nav-item ${location.pathname === '/login' ? 'active' : ''}`}>
-            <Link className="nav-link" to="/login">
-              <span className="orders">Login</span>
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+        <li className={`nav-item ${location.pathname === '/login' ? 'active' : ''}`}>
+          <Link className="nav-link" to="/login">
+            <span className="orders">Login</span>
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 };
-
-export default Sidebar;
