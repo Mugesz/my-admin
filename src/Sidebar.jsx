dbbdf6be5cd19cfd130c46249f5db6e87eb1f0f7
frekerzzzz <kannamugesh135@gmail.com>
2023-12-25 21:50:19 +0530

    mm

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -10,11 +10,6 @@ const Sidebar = () => {
       <button
         className="navbar-toggler"
         type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
       >
         <span className="navbar-toggler-icon"></span>
       </button>
